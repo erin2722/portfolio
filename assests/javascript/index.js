805bfa4988995a738834412913734af3012b1ad3
Erin <erin2722@gmail.com>
2020-07-05 18:06:45 -0400

    :sparkles: Done

--- assests/javascript/index.js
+++ assests/javascript/index.js
@@ -13,29 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-function submit() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const subject = document.getElementById("subject").value;
-  const message = document.getElementById("message").value;
+function toggleModal(project) {
+  console.log(project);
+  const modal = document.getElementById(project);
 
-  var service_id = "default_service";
-
-  const params = {
-    reply_to: email,
-    from_name: name,
-    subject,
-    message,
-  };
-
-  var template_id = "template_3HYPq8Hz";
-  console.log(service_id, params, template_id);
-  emailjs.send(service_id, template_id, params)
-  	.then(function(){ 
-       alert("Thanks for Reaching Out!");
-     }, function(err) {
-       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-    });
-
-  return false;
+  modal.classList.toggle('is-active');
 }
